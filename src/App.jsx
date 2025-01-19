c0cc68432694a52e782a7288d2fa45a223f4b669
Thayná Rie Shinoda <thayrieshinoda@gmail.com>
2025-01-18 23:39:09 -0300

    add .catch(), add buttonSearch, add responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
-
 import { useState } from 'react'
-import Perfil from './components/Perfil/index.jsx'
-import Formulario from './components/Formulario'
-import ReposList from './components/ReposList/index.jsx'
+import { Perfil } from './components/Perfil/Perfil.jsx'
+import { ReposList } from './components/ReposList/ReposList.jsx'
+import './global.css';
+import styles from './App.module.css'
 
 function App() {
   // const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true)
   const [nomeDoUsuario, setNomeDoUsuario] = useState('')
+
+  function handleSearch(event) {
+    event.preventDefault()
+    setNomeDoUsuario(event.target.inputSearch.value);
+  }
+
   return (
     <>
-      <input type="text" onBlur={(e) => setNomeDoUsuario(e.target.value)} />
-      
+      <div className={styles.searchArea}>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            name="inputSearch"
+            placeholder='Digite o nome do usuário'
+            className={styles.searchInput}
+            required
+          />
+          <button 
+            type="submit"
+            className={styles.searchButton}
+          >Search</button>
+          
+        </form>
+      </div>
+
       {nomeDoUsuario.length > 4 && (
         <>
           <Perfil nomeDoUsuario = {nomeDoUsuario}/>
           <ReposList nomeDoUsuario = {nomeDoUsuario}/>
         </>
       )}
-
-      {/* {formularioEstaVisivel && (
-        <Formulario/>
-      )}
-      
-      <button type="button" onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)}>toggle form</button> */}
     </>
   )
 }
--- src/components/ReposList/ReposList.jsx
+++ src/components/ReposList/ReposList.jsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
 import styles from './ReposList.module.css';
 
-export default function ReposList ({nomeDoUsuario}) {
+export function ReposList ({nomeDoUsuario}) {
   const [repos, setRepos] = useState([]);
   const [estaCarregando, setEstaCarregando] = useState(true);
+  const[erro, setErro] = useState('')
 
   useEffect(() => {
+    setRepos([]) //Limpa estados quando mudar de usuário
+    setErro('')
     setEstaCarregando(true)
     fetch(`https://api.github.com/users/${nomeDoUsuario}/repos`)
-    .then(res => res.json())
+    .then(res => {
+      if(!res.ok) {
+        throw new Error ('Usuário não encontrado.')
+      }
+      return res.json()
+    })
     .then(resJson => {
       setTimeout(() => {
         setEstaCarregando(false);
         setRepos(resJson);
       }, 3000)
+    })
+    .catch(err => {
+      setEstaCarregando(false)
+      setErro(err.message)
     })
   }, [nomeDoUsuario]);
 
@@ -21,8 +33,13 @@ export default function ReposList ({nomeDoUsuario}) {
 
     <div className="container">
       {estaCarregando ? (
-        <h1>Carregando...</h1>
-      ) : (
+        <h1 className={styles.carregandoMessage}>Carregando...</h1>
+      ) : erro ? (
+          <div className={styles.erroMessage}>
+            <h1>{erro}</h1>
+            <p>Por favor, verifique o nome do usuário e tente novamente.</p>
+          </div>
+        ) : (
         <ul className={styles.list}>
           {repos.map(repositorio => (
             <li key={repositorio.id} className={styles.listItem}>
@@ -35,10 +52,8 @@ export default function ReposList ({nomeDoUsuario}) {
               <a className={styles.itemLink} target="_blank" href={repositorio.html_url}>Visitar no github</a>
             </li>
           ))}
-        
         </ul>
       )}
-      
     </div>
   )
 }
